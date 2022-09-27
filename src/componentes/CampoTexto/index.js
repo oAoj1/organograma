1a34970893cd53b09b2920f9bfebf95cdea54642
oAoj1 <joaogbmarques@gmail.com>
2022-09-27 19:46:19 -0300

    Update index.js

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,6 @@
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
-    const placeholder_mod = `${props.placeholder}`
-    const label_mod = `${props.label}`
-    const valor_mod = `${props.valor}`
-    const obrigatorio = `${props.obrigatorio}`
 
     const Digitando = (evento) =>{
         props.Alterar(evento.target.value)
@@ -13,14 +9,14 @@ const CampoTexto = (props) => {
     return(
         <div className="campo-texto">
             <label>
-                {label_mod}
+                {props.label}
             </label>
 
             <input 
-                value={valor_mod} 
+                value={props.valor} 
                 onChange={Digitando}
-                required={obrigatorio} 
-                placeholder={placeholder_mod}
+                required={props.obrigatorio} 
+                placeholder={props.placeholder}
             />
         </div>
     )
