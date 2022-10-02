e636cdda704b0dd503803a5fd1fca85463ff332e
oAoj1 <joaogbmarques@gmail.com>
2022-10-02 09:56:24 -0300

    rodape

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,12 +1,24 @@
-const Rodape = (props) =>{
+import './Rodape.css'
+
+const Rodape = () =>{
     return(
         <footer>
             <div className="rodape-content">
-                <div className="redes-sociais">
+
+                <div className='redes-sociais'>
                     <img src="./imagens/fb.png"/>
                     <img src="./imagens/tw.png"/>
                     <img src="./imagens/ig.png"/>
                 </div>
+
+                <div className='logo-organo'>
+                    <img src="./imagens/logo.png"/>
+                </div>
+                    
+                <div className='descricao'>
+                    <p>Desenvolvido por <span>Alura</span>.</p>
+                </div>    
+                    
             </div>
         </footer>
     )
