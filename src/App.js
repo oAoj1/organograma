ec762c7ebfff0592b5c62800b964112d92f4f577
oAoj1 <joaogbmarques@gmail.com>
2022-10-03 09:57:42 -0300

    versoes finais

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   const [colaboradores,setColaboradores] = useState([])
 
   const ColaboradorAdicionado = (colaborador) =>{
+    debugger
     setColaboradores([...colaboradores,colaborador])
   }
 
