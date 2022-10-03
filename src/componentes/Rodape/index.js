bea76d88282a2f031ca335fd76c6e04c007ede87
oAoj1 <joaogbmarques@gmail.com>
2022-10-03 09:33:10 -0300

    arrumando rodape

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -3,22 +3,22 @@ import './Rodape.css'
 const Rodape = () =>{
     return(
         <footer>
-            <div className="rodape-content">
-
-                <div className='redes-sociais'>
-                    <img src="./imagens/fb.png"/>
-                    <img src="./imagens/tw.png"/>
-                    <img src="./imagens/ig.png"/>
+            <div className='rodape-content'>
+                <div className='social-medias'>
+                    <ul>
+                        <li><img src='./imagens/fb.png'/></li>
+                        <li><img src='./imagens/tw.png'/></li>
+                        <li><img src='./imagens/ig.png'/></li>
+                    </ul>   
                 </div>
 
                 <div className='logo-organo'>
-                    <img src="./imagens/logo.png"/>
+                    <img src='./imagens/logo.png'/>
                 </div>
-                    
+
                 <div className='descricao'>
-                    <p>Desenvolvido por <span>Alura</span>.</p>
-                </div>    
-                    
+                    <p>Desenvolvido por Alura.</p>
+                </div>
             </div>
         </footer>
     )
